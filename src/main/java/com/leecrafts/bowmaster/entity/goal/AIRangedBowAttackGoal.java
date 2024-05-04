9146d676eadebe72f62c4134fe8010dd8aa2af82
Wenhan-Samuel-Lee <wenhan_lee@utexas.edu>
2024-05-03 21:45:36 -0500

    adjusted rotation directions

--- src/main/java/com/leecrafts/bowmaster/entity/goal/AIRangedBowAttackGoal.java
+++ src/main/java/com/leecrafts/bowmaster/entity/goal/AIRangedBowAttackGoal.java
@@ -5,6 +5,7 @@ import com.leecrafts.bowmaster.capability.livingentity.ILivingEntityCap;
 import com.leecrafts.bowmaster.capability.livingentity.LivingEntityCap;
 import com.leecrafts.bowmaster.entity.custom.SkeletonBowMasterEntity;
 import com.leecrafts.bowmaster.util.NeuralNetworkUtil;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.Goal;
@@ -67,39 +68,40 @@ public class AIRangedBowAttackGoal<T extends SkeletonBowMasterEntity & RangedAtt
             float f1 = (float) (0.25 * f);
             this.mob.setSpeed(f1);
 
+
+            // just some precalculated values
+            Vec3 distance = livingEntity.position().subtract(this.mob.position());
+            Vec3 distanceNormalized = distance.normalize();
+            double pitchFacingTarget = Math.asin(-distanceNormalized.y); // in radians
+            double yawFacingTarget = Math.atan2(-distanceNormalized.x, distanceNormalized.z); // in radians
+
+            // observations and actions
             BasicNetwork network = this.mob.getNetwork();
 //            NeuralNetworkUtil.printWeights(network);
 //            network.getFlat().getWeights()[0] += 94321;
-            double[] observations = getObservations(livingEntity);
+            double[] observations = getObservations(livingEntity, distance, pitchFacingTarget, yawFacingTarget);
             double[] actionOutputs = NeuralNetworkUtil.computeOutput(network, observations);
-            handleRightClick(livingEntity, actionOutputs[0]);
-            handleMovement(actionOutputs[1], actionOutputs[2], actionOutputs[3]);
-            handleStrafing(actionOutputs[4], actionOutputs[5], actionOutputs[6]);
-            handleJump(actionOutputs[7]);
-            handleLookDirection(actionOutputs[8], actionOutputs[9]);
-
+            handleLookDirection(actionOutputs[0], actionOutputs[1], pitchFacingTarget, yawFacingTarget);
+//            handleLookDirection(0, 0, pitchFacingTarget, yawFacingTarget);
+            handleRightClick(livingEntity, actionOutputs[2]);
 //            spamArrows(livingEntity);
+            handleMovement(actionOutputs[3], actionOutputs[4], actionOutputs[5]);
+            handleStrafing(actionOutputs[6], actionOutputs[7], actionOutputs[8]);
+            handleJump(actionOutputs[9]);
+
         }
     }
 
-    public double[] getObservations(LivingEntity target) {
-        Vec3 agentPosition = this.mob.position();
-        Vec3 targetPosition = target.position();
-        Vec3 distance = targetPosition.subtract(agentPosition);
-        Vec3 distanceNormalized = distance.normalize();
-
+    public double[] getObservations(LivingEntity target, Vec3 distance, double pitchFacingTarget, double yawFacingTarget) {
         // Distances
         double horizontalDistance = Math.sqrt(distance.x * distance.x + distance.z * distance.z);
         double verticalDistance = distance.y;
 
         AtomicReference<Vec3> targetVelocity = new AtomicReference<>(Vec3.ZERO);
-        LazyOptional<ILivingEntityCap> capability = target.getCapability(ModCapabilities.LIVING_ENTITY_CAPABILITY);
-        if (capability.isPresent()) {
-            capability.ifPresent(iLivingEntityCap -> {
-                LivingEntityCap livingEntityCap = (LivingEntityCap) iLivingEntityCap;
-                targetVelocity.set(livingEntityCap.getVelocity());
-            });
-        }
+        target.getCapability(ModCapabilities.LIVING_ENTITY_CAPABILITY).ifPresent(iLivingEntityCap -> {
+            LivingEntityCap livingEntityCap = (LivingEntityCap) iLivingEntityCap;
+            targetVelocity.set(livingEntityCap.getVelocity());
+        });
         double[] target_FB_LR_UD = calculate_FB_LR_UD_ofVelocity(distance, targetVelocity.get());
         double target_fb = target_FB_LR_UD[0];
         double target_lr = target_FB_LR_UD[1];
@@ -108,8 +110,6 @@ public class AIRangedBowAttackGoal<T extends SkeletonBowMasterEntity & RangedAtt
         // TODO observe agent velocity and opponent's projectile velocity in order to help the agent dodge. (do later)
 
         // Differences in pitch and yaw
-        double pitchFacingTarget = Math.asin(distanceNormalized.y);
-        double yawFacingTarget = Math.atan2(distanceNormalized.x, distanceNormalized.z);
         double pitchDifference = pitchFacingTarget - Math.toRadians(this.mob.getXRot());
         double yawDifference = normalizeAngle(yawFacingTarget - Math.toRadians(this.mob.getYRot()));
 
@@ -195,18 +195,19 @@ public class AIRangedBowAttackGoal<T extends SkeletonBowMasterEntity & RangedAtt
         }
     }
 
-    private void handleLookDirection(double x, double y) {
-        // TODO normalize angle?
-        this.mob.setXRot((float) (360 * x));
-        this.mob.setYRot((float) (360 * y));
+    private void handleLookDirection(double xRotOffset, double yRotOffset, double pitchFacingTarget, double yawFacingTarget) {
+        this.mob.setXRot((float) Mth.clamp(Math.toDegrees(pitchFacingTarget) + 90 * yRotOffset, -90, 90));
+        this.mob.setYRot((float) Math.toDegrees(normalizeAngle(yawFacingTarget + Math.PI * xRotOffset)));
+//        this.mob.setXRot((float) (180 * x));
+//        this.mob.setYRot((float) (90 * y));
     }
 
-    private void spamArrows(LivingEntity target) {
+    private void spamArrows(LivingEntity target) { // for testing
         if (this.mob.isUsingItem()) {
             int i = this.mob.getTicksUsingItem();
             if (i >= 3) {
                 this.mob.stopUsingItem();
-                this.mob.performRangedAttack(target, BowItem.getPowerForTime(i));
+                this.mob.performRangedAttack(target, BowItem.getPowerForTime(20));
             }
         }
         else {
